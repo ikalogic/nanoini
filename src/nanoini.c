333ab6670b129dc0398b1797b22f85133b820fb9
ikalogic <contact@ikalogic.com>
2020-01-04 18:46:56 +0100

    initial commit

---
+++ src/nanoini.c
@@ -0,0 +1,130 @@
+/*
+ * Copyright (C) S.A.S. IKALOGIC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Ibrahim KAMAL <i.kamal@ikalogic.com>, May 2019
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+/*
+ * nanoini.c
+ *
+ *  Created on: 4 janv. 2020
+ *      Author: I.KAMAL
+ */
+
+#ifndef NANOINI_C_
+#define NANOINI_C_
+
+#include "string.h"
+#include "stdlib.h"
+#include "stdio.h"
+#include "stdint.h"
+#include "nanoini.h"
+
+void nanoini_reset_result(nanoini_parser_t *p);
+
+void nanoini_init(nanoini_parser_t *p)
+{
+    nanoini_reset_result(p);
+}
+
+void nanoini_feed_bloc(nanoini_parser_t *p,char* data, size_t len)
+{
+    p->s = NANOINI_PARSER_GET_KEY;
+    p->data_bloc_ptr = data;
+    p->data_bloc_end = data+len;
+}
+
+nanoini_result_t nanoini_parse_bloc(nanoini_parser_t *p)
+{
+    bool line_done = false;
+    p->result.valid = false;
+    char c;
+    while((p->data_bloc_ptr < p->data_bloc_end) && (line_done == false))
+    {
+        c = *(p->data_bloc_ptr);
+        switch (p->s)
+        {
+        case NANOINI_PARSER_GET_KEY:
+            if  (c == NANOINI_COMMENT)
+            {
+                p->s = NANOINI_PARSER_WAIT_NEW_LINE;
+            }
+            else if ((c == '\r')||(c == '\n'))
+            {
+                nanoini_reset_result(p);
+                //Skip those characters, simply.
+            }
+            else if ((c == NANOINI_DELIMITER))
+            {
+                p->s = NANOINI_PARSER_GET_VAL;
+                p->result.idx = 0;
+            }
+            else if (p->result.idx == NANOINI_MAX_KEY_LEN)
+            {
+                p->result.key_val_overflow = true;
+            }
+            else if (c != ' ')    //valid character for key (ignore spaces)
+            {
+                p->result.key[p->result.idx++] = c;
+            }
+            break;
+        case NANOINI_PARSER_GET_VAL:
+            if  ((c == NANOINI_COMMENT)||(c == '\r')||(c == '\n'))
+            {
+                line_done = true;
+                p->result.valid = true;
+                p->s = NANOINI_PARSER_WAIT_NEW_LINE;
+            }
+            else if (p->result.idx == NANOINI_MAX_VAL_LEN)
+            {
+                p->result.key_val_overflow = true;
+            }
+            else if (c != ' ')    //valid character for value (ignore spaces)
+            {
+                p->result.val[p->result.idx++] = c;
+            }
+            break;
+        case NANOINI_PARSER_WAIT_NEW_LINE:
+            if  ((c == '\r')||(c == '\n'))
+            {
+                nanoini_reset_result(p);
+                p->s = NANOINI_PARSER_GET_KEY;
+            }
+            break;    
+        default:
+            break;
+        }
+        p->data_bloc_ptr++;
+    }
+
+    if (p->data_bloc_ptr == p->data_bloc_end)
+    {
+        p->result.more = false;
+    }
+    else
+    {
+        p->result.more = true;
+    }
+
+    return p->result;
+}
+
+void nanoini_reset_result(nanoini_parser_t *p)
+{
+    p->result.idx = 0;
+    p->result.key_val_overflow=false;
+    memset(p->result.key,0,NANOINI_MAX_KEY_LEN);
+    memset(p->result.val,0,NANOINI_MAX_VAL_LEN);
+}
+
+#endif /* NANOINI_C_ */
+
+
---
+++ src/nanoini.h
@@ -0,0 +1,91 @@
+/*
+ * Copyright (C) S.A.S. IKALOGIC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Ibrahim KAMAL <i.kamal@ikalogic.com>, May 2019
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+/*
+ * nanoini.h
+ *
+ *  Created on: 4 janv. 2020
+ *      Author: I.KAMAL
+ *
+ *  This lib is meant to be *extremly* minimalistic, lightweight and efficient tool to
+ *  parse INI formated string on the fly, with absolute minimum memory footprint:
+ *  It only needs just enough memory to store a few pointers the the characters 
+ *  strings for a single key-value pair.
+ * 
+ *  Most importantnly: the memory usage is not related to maximum line size, unlike
+ *  most (all?) libraries available.
+ *  Comments are ignored without any impact on memory usage, regardless of its size
+ *  or quantity.
+ *
+ *  Features:
+ *  - Parses chunks of an ini file, whatever the chunk size, without impact on memory usage.
+ *  - Only needs to allocate enough memory for the key and the value strings
+ *  - Gracefully handles a key and/or value that does not fit in allocated space
+ *  - Doesn't need to allocate memory for a full line
+ *  - Works with lines ending with '\n', '\r' or any comnination of both.
+ *  - Only 3 functions interface
+ * 
+ * known limitations:
+ * - Key without value is not allowed
+ * - Delimiter and Comments character cannot be used for values,
+ *   even if the values are re "encapsulated".
+ * - Spaces in keys or values are simply ignored
+ * - Sections are not supported (simply ignored)
+ *
+ */
+
+#ifndef NANOINI_H_
+#define NANOINI_H_
+
+#include "stdbool.h"
+
+#define NANOINI_MAX_KEY_LEN 50
+#define NANOINI_MAX_VAL_LEN 50
+#define NANOINI_COMMENT     ';'
+#define NANOINI_DELIMITER   '='
+
+typedef struct
+{
+    char key[NANOINI_MAX_KEY_LEN];
+    char val[NANOINI_MAX_VAL_LEN];
+    int idx; //Index for both key and val
+    bool key_val_overflow;
+    bool valid;
+    bool more;  //function still needs to be called again.
+}nanoini_result_t;
+
+typedef enum
+{
+    NANOINI_PARSER_GET_KEY,
+    NANOINI_PARSER_GET_VAL,
+    NANOINI_PARSER_WAIT_NEW_LINE,
+}nanoini_state_t;
+
+typedef struct
+{
+    char* data_bloc_ptr;
+    char* data_bloc_end;
+    nanoini_result_t result;
+    nanoini_state_t s;
+}nanoini_parser_t;
+
+/**
+ * @brief nanoini_init need to be called before using the library
+ * @param p
+ */
+void nanoini_init(nanoini_parser_t *p);
+void nanoini_feed_bloc(nanoini_parser_t *p, char* data, size_t len);
+nanoini_result_t nanoini_parse_bloc(nanoini_parser_t *p);
+
+#endif /* NANOINI_H_ */
